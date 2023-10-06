f554a75e4d561ab051481a45ee7a4cf66d200ea7
martindipeco <martindipeco@gmail.com>
2023-10-06 11:16:36 -0300

    sigo instrucciones de Pablo

--- inmobiliaria_mddp/src/inmobiliaria_mddp/Inmobiliaria_mddp.java
+++ inmobiliaria_mddp/src/inmobiliaria_mddp/Inmobiliaria_mddp.java
@@ -11,6 +11,23 @@ public class Inmobiliaria_mddp {
     public static void main(String[] args) {
         // TODO code application logic here
         //Linea agregada solo para chequear push
+        
+        /**
+         * 
+          * 
+         */
+        
+        PropietarioMartin propi = new PropietarioMartin(1, 3333);
+        InquilinoMartin inqui = new InquilinoMartin(1);
+        InmuebleMartin inmu = new InmuebleMartin(1, propi);
+        
+       
+        Calquiler alquilo = new Calquiler(LocalDate.of(2023, 12, 6), LocalDate.of(2023, 12, 7), 777, 1,inmu, inqui);
+        
+        CalquilerData alquiData = new CalquilerData();
+        
+        alquiData.generarContrato(alquilo);
+        
     }
     
 }
---
+++ inmobiliaria_mddp/src/inmobiliaria_mddp/entidades/InquilinoMartin.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inmobiliaria_mddp.entidades;
+
+/**
+ *
+ * @author marti
+ */
+public class InquilinoMartin {
+    
+    private int id_inquilino;
+
+    public InquilinoMartin(int id_inquilino) {
+        this.id_inquilino = id_inquilino;
+    }
+    
+    
+    
+    public int getId_inquilino() {
+        return id_inquilino;
+    }
+    
+}
