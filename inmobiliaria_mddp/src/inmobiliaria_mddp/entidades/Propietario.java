16bc7fa0fb34e12781dc67920178e9cecdd833d0
Dimoda22 <naelpa@hotmail.com>
2023-10-09 10:15:41 -0300

    otra vez

--- inmobiliaria_mddp/src/inmobiliaria_mddp/entidades/Propietario.java
+++ inmobiliaria_mddp/src/inmobiliaria_mddp/entidades/Propietario.java
@@ -33,11 +33,71 @@ public class Propietario {
         this.mail = mail;
     }
 
-    @Override
+    public int getIdPropietario() {
+        return idPropietario;
+    }
+
+    public void setIdPropietario(int idPropietario) {
+        this.idPropietario = idPropietario;
+    }
+
+    public int getCuit() {
+        return cuit;
+    }
+
+    public void setCuit(int cuit) {
+        this.cuit = cuit;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDomicilio() {
+        return domicilio;
+    }
+
+    public void setDomicilio(String domicilio) {
+        this.domicilio = domicilio;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+     @Override
     public String toString() {
         return "Propietario{" + "idPropietario=" + idPropietario + ", cuit=" + cuit + ", apellido=" + apellido + ", nombre=" + nombre + ", domicilio=" + domicilio + ", telefono=" + telefono + ", mail=" + mail + '}';
+        
     }
     
-    
-    
 }
+    
+    
+    
+    
+
